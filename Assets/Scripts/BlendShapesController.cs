9b40ff7ea37b6de6ebe326bbaceaeecb524d15ff
James <wtx666666@hrbeu.edu.cn>
2019-12-19 23:03:39 +0800

    Add mouth length control

--- Assets/Scripts/BlendShapesController.cs
+++ Assets/Scripts/BlendShapesController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class BlendShapesController : MonoBehaviour
 {
+    public int leftEyeNum;
+    public int rightEyeNum;
+    public int mouthWidNum;
+    public int mouthLenNum;
+    public int shockEyeNum;
+
     SkinnedMeshRenderer skinnedMeshRenderer;
     Mesh skinnedMesh;
     public static Vector3 leftEyeShape;
@@ -25,57 +31,70 @@ public class BlendShapesController : MonoBehaviour
         // Set blend shape for left eye
         if(leftEyeShape[1]<0.05f)//闭眼 //参数匹配：ParameterServer -> 参数控制!闭眼
         {
-            skinnedMeshRenderer.SetBlendShapeWeight (2, 100);
+            skinnedMeshRenderer.SetBlendShapeWeight (leftEyeNum, 100);
         }
         else if(leftEyeShape[1]<0.1f)
         {
-            skinnedMeshRenderer.SetBlendShapeWeight (2, 5/leftEyeShape[1]);
+            skinnedMeshRenderer.SetBlendShapeWeight (leftEyeNum, 5/leftEyeShape[1]);
             
         }
         else if(leftEyeShape[1]<0.2f)
         {
-            skinnedMeshRenderer.SetBlendShapeWeight (2, -500*leftEyeShape[1]+100);  
+            skinnedMeshRenderer.SetBlendShapeWeight (leftEyeNum, -500*leftEyeShape[1]+100);  
             //Debug.Log(leftEyeShape[1]);
         }
         else
         {
-            skinnedMeshRenderer.SetBlendShapeWeight (2, 0);
+            skinnedMeshRenderer.SetBlendShapeWeight (leftEyeNum, 0);
         }
         
         // Set blend shape for right eye
         if(rightEyeShape[1]<0.05f)
         {
-            skinnedMeshRenderer.SetBlendShapeWeight (3, 100);
+            skinnedMeshRenderer.SetBlendShapeWeight (rightEyeNum, 100);
         }
         else if(rightEyeShape[1]<0.1f)
         {
-            skinnedMeshRenderer.SetBlendShapeWeight (3, 5/rightEyeShape[1]);
+            skinnedMeshRenderer.SetBlendShapeWeight (rightEyeNum, 5/rightEyeShape[1]);
             
         }
         else if(rightEyeShape[1]<0.2f)
         {
             
-            skinnedMeshRenderer.SetBlendShapeWeight (3, -500*rightEyeShape[1]+100);
+            skinnedMeshRenderer.SetBlendShapeWeight (rightEyeNum, -500*rightEyeShape[1]+100);
         }
         else
         {
-            skinnedMeshRenderer.SetBlendShapeWeight (3, 0);
+            skinnedMeshRenderer.SetBlendShapeWeight (rightEyeNum, 0);
         }
 
         // Shocked
         if(rightEyeShape[1]>0.25f)//惊愕 0.25-0.35线性
         {
-            skinnedMeshRenderer.SetBlendShapeWeight (29, 500*rightEyeShape[1]-125);
+            skinnedMeshRenderer.SetBlendShapeWeight (shockEyeNum, 500*rightEyeShape[1]-125);
         }
         else
         {
-            skinnedMeshRenderer.SetBlendShapeWeight (29, 0);
+            skinnedMeshRenderer.SetBlendShapeWeight (shockEyeNum, 0);
         }
 
         // Set blend shape for mouth
         if (500*mouthShape[1] < 100f)
         {
-            skinnedMeshRenderer.SetBlendShapeWeight (9, 500*mouthShape[1]);
+            skinnedMeshRenderer.SetBlendShapeWeight (mouthWidNum, 500*mouthShape[1]);
+        }
+
+        if(mouthShape[0] <0.1f)
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight (mouthLenNum, 50);
+        }
+        else if(mouthShape[0] < 0.4f)
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight (mouthLenNum, 120f-400*mouthShape[0]);
+        }
+        else
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight (mouthLenNum, 0);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-# Open Virtual Head
-A virtual head system for VTubers in Python and Unity with smooth motion and robust facial expressions
+# OpenVHead
+
+**Author**: [Tianxing Wu](https://github.com/TianxingWu)
+
+<p align="center">
+    <img src="./Doc/OpenVHead.jpg">
+</p>
+
+## Introduction
+
+This is an on-going project to build a virtual head system for **VTubers**. To make the animation looks more authentic, several filters and control methods are used to smooth and enhance the robustness of the head motion and facial expressions. The system is mainly build with C# and Python in Unity3D environment.
+
+Please feel free to add new functions to this project or just play around for entertainment. To cite this repo, please use name "OpenVHead" with the project link [here](https://github.com/TianxingWu/OpenVHead).
+
+
+## Prerequisite
+
+### Hardware
+- a PC with web camera
+
+### Software
+- 
+
 
 Updating ...
