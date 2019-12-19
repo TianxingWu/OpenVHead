faacdadda60a474b6660057fa5bca86d247a1998
James <wtx666666@hrbeu.edu.cn>
2019-12-19 21:32:17 +0800

    Update code comments

--- Assets/Scripts/BlendShapesController.cs
+++ Assets/Scripts/BlendShapesController.cs
@@ -22,53 +22,47 @@ public class BlendShapesController : MonoBehaviour
 
     void Update ()
     {
-        //左眼形变
-        // if (1/leftEyeShape[1] < 100f) 
-        // {
-            if(leftEyeShape[1]<0.05f)//闭眼 //参数匹配：ParameterServer -> 参数控制!闭眼
-            {
-                skinnedMeshRenderer.SetBlendShapeWeight (2, 100);
-            }
-            else if(leftEyeShape[1]<0.1f)
-            {
-                skinnedMeshRenderer.SetBlendShapeWeight (2, 5/leftEyeShape[1]);
-                
-            }
-            else if(leftEyeShape[1]<0.2f)
-            {
-                skinnedMeshRenderer.SetBlendShapeWeight (2, -500*leftEyeShape[1]+100);  
-                //Debug.Log(leftEyeShape[1]);
-            }
-            else
-            {
-                skinnedMeshRenderer.SetBlendShapeWeight (2, 0);
-            }
-        // }
+        // Set blend shape for left eye
+        if(leftEyeShape[1]<0.05f)//闭眼 //参数匹配：ParameterServer -> 参数控制!闭眼
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight (2, 100);
+        }
+        else if(leftEyeShape[1]<0.1f)
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight (2, 5/leftEyeShape[1]);
+            
+        }
+        else if(leftEyeShape[1]<0.2f)
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight (2, -500*leftEyeShape[1]+100);  
+            //Debug.Log(leftEyeShape[1]);
+        }
+        else
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight (2, 0);
+        }
         
-        //右眼形变
-        // if (1/rightEyeShape[1] < 100f) 
-        // {
-            if(rightEyeShape[1]<0.05f)
-            {
-                skinnedMeshRenderer.SetBlendShapeWeight (3, 100);
-            }
-            else if(rightEyeShape[1]<0.1f)
-            {
-                skinnedMeshRenderer.SetBlendShapeWeight (3, 5/rightEyeShape[1]);
-                
-            }
-            else if(rightEyeShape[1]<0.2f)
-            {
-                
-                skinnedMeshRenderer.SetBlendShapeWeight (3, -500*rightEyeShape[1]+100);
-            }
-            else
-            {
-                skinnedMeshRenderer.SetBlendShapeWeight (3, 0);
-            }
-        // }
+        // Set blend shape for right eye
+        if(rightEyeShape[1]<0.05f)
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight (3, 100);
+        }
+        else if(rightEyeShape[1]<0.1f)
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight (3, 5/rightEyeShape[1]);
+            
+        }
+        else if(rightEyeShape[1]<0.2f)
+        {
+            
+            skinnedMeshRenderer.SetBlendShapeWeight (3, -500*rightEyeShape[1]+100);
+        }
+        else
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight (3, 0);
+        }
 
-        //惊愕
+        // Shocked
         if(rightEyeShape[1]>0.25f)//惊愕 0.25-0.35线性
         {
             skinnedMeshRenderer.SetBlendShapeWeight (29, 500*rightEyeShape[1]-125);
@@ -78,7 +72,7 @@ public class BlendShapesController : MonoBehaviour
             skinnedMeshRenderer.SetBlendShapeWeight (29, 0);
         }
 
-
+        // Set blend shape for mouth
         if (500*mouthShape[1] < 100f)
         {
             skinnedMeshRenderer.SetBlendShapeWeight (9, 500*mouthShape[1]);
